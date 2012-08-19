ec63d1b023f0c74f16368e7dc2c7e8c576a21867
Cedric Boverie <ced@boverie.eu>
2012-08-19 19:13:03 +0200

    Custom objects of my plugins are now inclued

--- lib/siri_objects.rb
+++ lib/siri_objects.rb
@@ -326,3 +326,38 @@ add_property_to_class(SiriSetRequestOrigin, :longitude)
 add_property_to_class(SiriSetRequestOrigin, :verticalAccuracy)
 add_property_to_class(SiriSetRequestOrigin, :direction)
 add_property_to_class(SiriSetRequestOrigin, :age)
+
+
+# Custom Objects and Commands
+
+class OpenLink < SiriObject
+  def initialize(ref="")
+	super("OpenLink", "com.apple.ace.assistant")
+	self.ref = ref
+  end
+end
+add_property_to_class(OpenLink, :ref)
+
+class ClientBoundCommand < SiriObject
+	def initialize(encodedClassName, groupIdentifier, aceId=nil, refId=nil, callbacks=[])
+		super(encodedClassName, groupIdentifier)
+		self.aceId= aceId
+		self.refId = refId
+		self.callbacks = callbacks
+	end
+end
+add_property_to_class(ClientBoundCommand, :aceId)
+add_property_to_class(ClientBoundCommand, :refId)
+add_property_to_class(ClientBoundCommand, :callbacks)
+
+class WebSearch < ClientBoundCommand
+	def initialize(refId=nil, aceId=nil, query="", provider="Default", targetAppId="")
+		super("Search", "com.apple.ace.websearch", aceId, refId)
+		self.query = query
+		self.provider = provider
+		self.targetAppId = targetAppId
+	end
+end
+add_property_to_class(WebSearch, :query)
+add_property_to_class(WebSearch, :provider)
+add_property_to_class(WebSearch, :targetAppId)
